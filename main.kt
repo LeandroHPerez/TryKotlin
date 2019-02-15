42592059b97296b27335bb322cefa1294ffd90f4
Leandro H. Perez <leandropremier@gmail.com>
2019-02-15 21:18:48 -0200

    Exemplo simples de Kotlin

---
+++ main.kt
@@ -0,0 +1,22 @@
+package br.edu.ifsp.scl.sdm.trykotlin
+
+fun main(args: Array<String>) {
+
+    //Declaração de variável mutável
+    var frase: String = "Hello, World!"
+
+
+    println("Hello, world!")
+
+    println(frase)
+
+    println("$frase")
+    println("${frase}")
+
+    //Com chamada de função
+    println("Quantidade de caracteres: ${frase.count()}")
+
+    //Com propriedade (get)
+    println("Quantidade de caracteres: ${frase.length}")
+
+}
