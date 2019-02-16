d84a34d29895c6bc173aa954a5f72162b3de60f6
Leandro H. Perez <leandropremier@gmail.com>
2019-02-15 22:23:31 -0200

    Exercício 1

---
+++ Exercicio 1.kt
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.sdm.trykotlin
+
+fun main(args: Array<String>) {
+
+
+    val listaCursos: MutableList<String> = mutableListOf("Sistemas para dispositivos móveis", "Análise e Desenvolvimento de Sistemas")
+
+    listaCursos.add("Técnico em Informática para Internet")
+    listaCursos.add("Manutenção de Aeronaves")
+    listaCursos.add("Técnico em Células")
+    listaCursos.add("Processos gerenciais")
+
+
+}
