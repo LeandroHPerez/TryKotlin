368d2540abf605739932c48850550f4675ffdb4b
Leandro H. Perez <leandropremier@gmail.com>
2019-02-15 22:40:42 -0200

    Exercício 1 - resolução 1, 2 e 3

--- Exercicio 1.kt
+++ Exercicio 1.kt
@@ -3,12 +3,53 @@ package br.edu.ifsp.scl.sdm.trykotlin
 fun main(args: Array<String>) {
 
 
-    val listaCursos: MutableList<String> = mutableListOf("Sistemas para dispositivos móveis", "Análise e Desenvolvimento de Sistemas")
+    val listaCursos: MutableList<String> = mutableListOf("Sistemas para Dispositivos Móveis", "Análise e Desenvolvimento de Sistemas")
 
     listaCursos.add("Técnico em Informática para Internet")
     listaCursos.add("Manutenção de Aeronaves")
     listaCursos.add("Técnico em Células")
-    listaCursos.add("Processos gerenciais")
+    listaCursos.add("Processos Gerenciais")
+
+    println("Solução 1")
+
+    //Solução 1
+    val listaCursosSistemas = mutableListOf<String>()
+
+    for(curso in listaCursos){
+        if (curso.contains("Sistemas")){
+            listaCursosSistemas.add(curso)
+        }
+    }
+
+    for (i in 0..listaCursosSistemas.size -1){
+        println("$i - ${listaCursosSistemas[i]}")
+    }
+
+
+    println("Solução 2")
+
+    //Solução 2
+    val listaCursosSistemasSol2 = listaCursos.filter { it.contains("Sistemas") }
+    //listaCursosSistemasSol2.withIndex().forEach { println(it) }
+
+    listaCursosSistemasSol2.withIndex().forEach { println("${it.index} - ${it.value}") }
+
+
+
+    println("Solução 3")
+
+    //Solução 3
+    fun imprime(ci: IndexedValue<String>){
+        println("${ci.index} - ${ci.value}")
+
+    }
+
+    val listaCursosSistemasSol3 = listaCursos.filter { it.contains("Sistemas") }
+    //listaCursosSistemasSol3.withIndex().forEach { println(it) }
+
+    listaCursosSistemasSol3.withIndex().forEach (::imprime) //passa a função como parâmetro
+
+
 
 
 }
