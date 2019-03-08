2740a22396227a345fc7495eee94b10671f3cb53
Leandro H. Perez <leandropremier@gmail.com>
2019-03-08 20:21:43 -0300

    Adição de material das aulas em exercícios

--- Exercicio 3.kt
+++ Exercicio 3.kt
@@ -10,7 +10,97 @@ fun main() {
 }
 
 /*
-Exercício: a saída deve ser
+Exercício: utilizando uma função de alta ordem que receba das funções dadas anterioremente (foo e bar)
+a saída deve ser :
         30
         Pedro
 */
+
+fun foo(x: Int, y: Int, f:(Int, Int) -> Int):Int = f(x,y)
+
+fun bar(x: String, y: String, g:(String, String) -> String):String = g(x,y)
+
+
+
+fun main() {
+    println(xpto(2, 3, ::soma))
+    println(xpto("Jo", "ão", ::cat))
+}
+
+/*
+Exercício: utilizando uma função de alta ordem que receba das funções dadas anterioremente (foo e bar)
+a saída deve ser :
+        5
+        João
+*/
+
+
+fun <T,U> xpto(x: T, y: U, f:(T, U) -> T): T = f(x,y)
+
+fun bar (x: String, y: Int): String = "${x}${y}"
+
+xpto("Pedro", 36, ::bar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Funções de alta ordem
+fun processaInteiro(i: Int, f: (Int) -> Int): Int{
+    return f(i)
+}
+
+fun processaInteiros(i: Int, j: Int,  f: (Int, Int) -> Int): Int{
+    return f(i, j)
+}
+
+
+//funções
+fun soma(i: Int, j: Int): Int{
+    return i + j
+}
+
+fun multiplica(i: Int, j: Int): Int{
+    return i * j
+}
+
+
+fun raizQuadrada(numero: Int): Int{
+    for (n in 1..numero){
+        if (n * n == numero)
+            return n
+    }
+    return -1
+}
+
+fun main() {
+    //1. Chamadas do primeiro passo
+    println(processaInteiros(10, 20, ::soma))
+    println(processaInteiros(10, 20, ::multiplica))
+    println(processaInteiro(16,  ::raizQuadrada))
+
+    //2. Chamadas do segundo
+    println(processaInteiros(10, 20, ::multiplica))
+
+    println(processaInteiros(10, 20, ::multiplica))
+}
+
+
+
+
+
+
+
+
+
+
+
